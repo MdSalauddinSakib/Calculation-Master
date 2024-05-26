80a5f4995edd9155941e07e758e59cf44e572e07
Md Salauddin Sakib <salauddinsakib99@gmail.com>
2024-05-27 00:03:16 +0600

    triangle area calculator create done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calculate () {
+function calculate() {
     const num1 = parseFloat(document.getElementById("num1").value);
     const num2 = parseFloat(document.getElementById("num2").value);
     const operation = document.getElementById("operation").value;
@@ -15,9 +15,9 @@ function calculate () {
             result = num1 * num2;
             break;
         case 'division':
-            if(num2 !== 0){
+            if (num2 !== 0) {
                 result = num1 / num2;
-            }else{
+            } else {
                 result = 'Cannot divide by zero';
             }
             break;
@@ -32,3 +32,18 @@ function calculate () {
 }
 
 // calculate triangle area
+const calculateTriangleArea = () => {
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+
+    const triangleHeightValue = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightValue.value;
+    const height = parseFloat(triangleHeightText);
+
+    const triangleArea = 0.5 * base * height;
+
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = triangleArea;
+
+}
