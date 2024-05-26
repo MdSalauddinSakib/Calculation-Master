88615cba21071a988cb4d5e7eaed5a204c641bcf
Md Salauddin Sakib <salauddinsakib99@gmail.com>
2024-05-26 18:05:52 +0600

    basic calculator css add done

---
+++ script.js
@@ -0,0 +1,5 @@
+const calculate = ()=>{
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const operation = document.getElementById("operation").value;
+}
