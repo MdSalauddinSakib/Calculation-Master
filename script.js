6c23a828a6909f2abc38e6e2bfe8e6881afbcc4c
Md Salauddin Sakib <salauddinsakib99@gmail.com>
2024-05-26 23:21:50 +0600

    triangle area css add done

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-const calculate = ()=>{
+function calculate () {
     const num1 = parseFloat(document.getElementById("num1").value);
     const num2 = parseFloat(document.getElementById("num2").value);
     const operation = document.getElementById("operation").value;
+    let result;
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtraction':
+            result = num1 - num2;
+            break;
+        case 'multiplication':
+            result = num1 * num2;
+            break;
+        case 'division':
+            if(num2 !== 0){
+                result = num1 / num2;
+            }else{
+                result = 'Cannot divide by zero';
+            }
+            break;
+        case 'remainder':
+            result = num1 % num2;
+            break;
+        default:
+            result = 'Invalid operation';
+    }
+
+    document.getElementById('result').innerText = `Result: ${result}`;
 }
